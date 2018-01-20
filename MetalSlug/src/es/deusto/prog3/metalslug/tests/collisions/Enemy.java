b1645e3dcd07203b419122c3e8abaefa2fcb7188
kuu22 <pabloalonso998@gmail.com>
2018-01-20 11:44:43 +0100

    Arreglado concurrentModification (hilos = mal)

--- MetalSlug/src/es/deusto/prog3/metalslug/tests/collisions/Enemy.java
+++ MetalSlug/src/es/deusto/prog3/metalslug/tests/collisions/Enemy.java
@@ -4,38 +4,29 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Enemy extends Character {
-	
+
 	private Player player;
-	private Thread updateShooting;
 	private boolean shooting;
 	private Thread changeDirection;
 	private boolean movingLeft;
 	private boolean dead;
+	private int timeCounter;
 
 	public Enemy(float x, float y, Player player) {
 		super(x, y, 30, 30, 100);
 		this.player = player;
 		dead = false;
-		updateShooting = new Thread(() -> {
-			while(true) {
-				if(distanceTo(this.getCenter(), player.getCenter()) < 500)
-					shoot();
-				try {
-					Thread.sleep(500);
-				} catch (InterruptedException e) { }
-			}
-		});
-		
+
 		changeDirection = new Thread(() -> {
 			movingLeft = true;
-			while(true) {
+			while (true) {
 				movingLeft = !movingLeft;
 				try {
 					Thread.sleep(2000);
-				} catch (InterruptedException e) { }
+				} catch (InterruptedException e) {
+				}
 			}
 		});
-		updateShooting.start();
 		changeDirection.start();
 	}
 
@@ -47,26 +38,33 @@ public class Enemy extends Character {
 		// TODO Auto-generated method stub
 		return (float) Math.hypot(a[0] - b[0], a[1] - b[1]);
 	}
-	
+
 	public void update(int delta, ArrayList<Bullet> playerbullets, ArrayList<Granada> granadas) {
+		timeCounter += delta;
 		moveX(delta, movingLeft);
-		
+
 		moveY(delta);
 		detectPlatformCollisions();
-		for(Iterator<Bullet> iterator = playerbullets.iterator(); iterator.hasNext();) {
+		for (Iterator<Bullet> iterator = playerbullets.iterator(); iterator.hasNext();) {
 			Bullet b = iterator.next();
-			if(this.contains(b.getX(), b.getY())) {
+			if (this.contains(b.getX(), b.getY())) {
 				dead = true;
 			}
 		}
-		for(Iterator<Granada> iterator = granadas.iterator(); iterator.hasNext();) {
+		for (Iterator<Granada> iterator = granadas.iterator(); iterator.hasNext();) {
 			Granada g = iterator.next();
-			if(this.intersects(g)) {
+			if (this.intersects(g)) {
 				dead = true;
 			}
 		}
+
+		if (timeCounter > 1000 && distanceTo(this.getCenter(), player.getCenter()) < 500) {
+			shoot();
+			timeCounter = 0;
+		}
+
 	}
-	
+
 	public boolean isDead() {
 		return dead;
 	}
