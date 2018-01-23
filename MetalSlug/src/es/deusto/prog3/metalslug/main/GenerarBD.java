0b52e14005c037f226147b0da29088d712aa5335
ahrPope <alvaroh@opendeusto.es>
2018-01-23 19:27:59 +0100

    Format

--- MetalSlug/src/es/deusto/prog3/metalslug/main/GenerarBD.java
+++ MetalSlug/src/es/deusto/prog3/metalslug/main/GenerarBD.java
@@ -39,31 +39,32 @@ public class GenerarBD {
 		platforms.add(new Slope(1274, 560, 1716, 680));
 		platforms.add(new Platform(0, 640, 1500, 80, false));
 		platforms.add(new Platform(-20, 0, 20, 720, false));
+		
 		BaseDeDatos.guardarPlataformas(platforms, 1);
 
 		// Nivel 2
 		platforms.clear();
 		platforms.add(new Platform(0, 563, 865, 40, false));
-        	platforms.add(new Slope(642, 580, 875, 489));
-        	platforms.add(new Platform(0, 601, 7232, 40, false));
-        	platforms.add(new Platform(1300, 489, 137, 100, false));
-        	platforms.add(new Platform(1729, 489, 178, 100, false));
-        	platforms.add(new Platform(2310, 489, 720, 100, false));
-        	platforms.add(new Slope(3684, 579, 3456, 489));
-        	platforms.add(new Platform(3456, 579, 1690, 40, false));
-        	platforms.add(new Slope(4964, 589, 5175, 480));
-        	platforms.add(new Platform(5997, 489, 175, 100, false));
-        	platforms.add(new Platform(6581, 489, 656, 100, false));
-        	platforms.add(new Platform(4133, 437, 189, 10, true));
-        	platforms.add(new Platform(4516, 437, 189, 10, true));
+		platforms.add(new Slope(642, 580, 875, 489));
+		platforms.add(new Platform(0, 601, 7232, 40, false));
+		platforms.add(new Platform(1300, 489, 137, 100, false));
+		platforms.add(new Platform(1729, 489, 178, 100, false));
+		platforms.add(new Platform(2310, 489, 720, 100, false));
+		platforms.add(new Slope(3684, 579, 3456, 489));
+		platforms.add(new Platform(3456, 579, 1690, 40, false));
+		platforms.add(new Slope(4964, 589, 5175, 480));
+		platforms.add(new Platform(5997, 489, 175, 100, false));
+		platforms.add(new Platform(6581, 489, 656, 100, false));
+		platforms.add(new Platform(4133, 437, 189, 10, true));
+		platforms.add(new Platform(4516, 437, 189, 10, true));
 		platforms.add(new Platform(-20, 0, 20, 720, false));
 
 		BaseDeDatos.guardarPlataformas(platforms, 2);
-		
-		//Enemigos
-		//Nivel1
 
-		BaseDeDatos.guardarEnemigo(810, 600, 600, 1140,1);
+		// Enemigos
+		// Nivel1
+
+		BaseDeDatos.guardarEnemigo(810, 600, 600, 1140, 1);
 		BaseDeDatos.guardarEnemigo(1200, 600, 945, 1500, 1);
 		BaseDeDatos.guardarEnemigo(1605, 600, 1290, 1920, 1);
 		BaseDeDatos.guardarEnemigo(1995, 600, 1890, 2070, 1);
@@ -88,19 +89,19 @@ public class GenerarBD {
 		BaseDeDatos.guardarEnemigo(7200, 600, 6900, 7500, 1);
 		BaseDeDatos.guardarEnemigo(7500, 600, 6600, 7800, 1);
 
-		//Nivel2
-		BaseDeDatos.guardarEnemigo(1314,470,1314,1423, 2);
-		BaseDeDatos.guardarEnemigo(2397,470,2397,2925, 2);
-		BaseDeDatos.guardarEnemigo(2925,470,2397,2925, 2);
-		BaseDeDatos.guardarEnemigo(3462,470,3462,3675, 2);
-		BaseDeDatos.guardarEnemigo(4125,300,4125,4302, 2);
-		BaseDeDatos.guardarEnemigo(4503,300,4503,4692, 2);
-		BaseDeDatos.guardarEnemigo(6726,470,6726,7143, 2);
-		BaseDeDatos.guardarEnemigo(921,550,921,1220, 2);
-		BaseDeDatos.guardarEnemigo(1476,550,1476,1638, 2);
-		BaseDeDatos.guardarEnemigo(1953,550,1953,2193, 2);
-		BaseDeDatos.guardarEnemigo(5187,550,5187,5889, 2);
-		BaseDeDatos.guardarEnemigo(6249,550,1314,6468, 2);
+		// Nivel2
+		BaseDeDatos.guardarEnemigo(1314, 470, 1314, 1423, 2);
+		BaseDeDatos.guardarEnemigo(2397, 470, 2397, 2925, 2);
+		BaseDeDatos.guardarEnemigo(2925, 470, 2397, 2925, 2);
+		BaseDeDatos.guardarEnemigo(3462, 470, 3462, 3675, 2);
+		BaseDeDatos.guardarEnemigo(4125, 300, 4125, 4302, 2);
+		BaseDeDatos.guardarEnemigo(4503, 300, 4503, 4692, 2);
+		BaseDeDatos.guardarEnemigo(6726, 470, 6726, 7143, 2);
+		BaseDeDatos.guardarEnemigo(921, 550, 921, 1220, 2);
+		BaseDeDatos.guardarEnemigo(1476, 550, 1476, 1638, 2);
+		BaseDeDatos.guardarEnemigo(1953, 550, 1953, 2193, 2);
+		BaseDeDatos.guardarEnemigo(5187, 550, 5187, 5889, 2);
+		BaseDeDatos.guardarEnemigo(6249, 550, 1314, 6468, 2);
 	}
 
 }
