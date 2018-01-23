c4afce3d7518d53ebe4b9202ab02d98683a463cb
Egoitz <egoes98@gmail.com>
2018-01-23 18:25:53 +0100

    Add files via upload
    Llama al método que borra la base de datos.

---
+++ MetalSlug/src/es/deusto/prog3/metalslug/basededatos/BoraraBaseDeDatos.java
@@ -0,0 +1,10 @@
+package es.deusto.prog3.metalslug.basededatos;
+
+public class BoraraBaseDeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BaseDeDatos.borrar();
+	}
+
+}
