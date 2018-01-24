c7aad0636b637689fcc5e8351db8eecdd21fd0a0
Egoitz <egoes98@gmail.com>
2018-01-24 12:34:07 +0100

    Update FPuntuacion.java
    Ya esta echa la de puntuaciones.

--- MetalSlug/src/es/deusto/prog3/metalslug/main/FPuntuacion.java
+++ MetalSlug/src/es/deusto/prog3/metalslug/main/FPuntuacion.java
@@ -23,7 +23,7 @@ import es.deusto.prog3.metalslug.basededatos.BaseDeDatos;
 import es.deusto.prog3.metalslug.game.entities.Player;
 
 public class FPuntuacion extends BasicGameState{
-	Image fondo_fs, m_ij;
+	Image fondo_fs, m_ij, n_best, b_comprobacion, texto;
 	
 	int mouseY;
 	int mouseX;
@@ -43,7 +43,7 @@ public class FPuntuacion extends BasicGameState{
 	public FPuntuacion(int state, Player player) {
 		super();
 		this.player = player;
-		this.score = player.getScore();
+		this.score = score;
 		// TODO Auto-generated constructor stub
 
 	}
@@ -53,15 +53,18 @@ public class FPuntuacion extends BasicGameState{
 		fondo_fs = new Image ("resources/Menu/Menu_fp/fondo_fp.png");
 		input = gc.getInput();
 
-		scorefont = new UnicodeFont("resources/mini_pixel-7.TTF", 40, false, false);
+		scorefont = new UnicodeFont("resources/ARCADECLASSIC.TTF", 40, false, false);
 		scorefont.addAsciiGlyphs();
 		scorefont.getEffects().add(new ColorEffect(java.awt.Color.WHITE));
 		scorefont.loadGlyphs();
 		
+		b_comprobacion = new Image("resources/Menu/Menu_fp/b_comprobacion.png");
+		texto = new Image("resources/Menu/Menu_fp/texto.png");
 		m_ij = new Image("resources/Menu/Menu_fp/m_ij.png");
-		nJugador = new TextField(gc, scorefont, 501, 441, 250, 40);
+		n_best = new Image("resources/Menu/Menu_fp/newBest.png");
+		nJugador = new TextField(gc, scorefont, 510, 388, 250, 40);
 		tF = true;
-		mIJ = true;
+		mIJ = false;
 		mC = false;
 		nB = false;
 	}
@@ -72,25 +75,30 @@ public class FPuntuacion extends BasicGameState{
 		
 		scorefont.drawString(760, 433, String.format("%06d", player.getScore()));
 		
-		if (mIJ) {
+		if (!mIJ) {
 			Color trans = new Color(0f, 0f, 0f, 0.5f);
 			g.setColor(trans);
 			g.fillRect(0, 0, 1280, 720);
-			m_ij.draw();
+			m_ij.draw(280,196);
 			if(tF) {
+				g.setColor(Color.white);
 				nJugador.render(gc, g);
 			}else {
-				
+				texto.draw(402,314);
+				b_comprobacion.draw(310,448);
 			}
 		}else {
-			//Temporal
 			scorefont.drawString(304, 433, String.format("%06d", BaseDeDatos.getPuntuacionJ(jugador)));
+		}
+		
+		if(nB) {
+			n_best.draw(430, 271);
 		}
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
-		if(!mIJ) {
+		if(mIJ) {
 			if(input.getMouseX() >= 427 && input.getMouseX() <= 798 && input.getMouseY() >= 628
 					&& input.getMouseY() <= 706){
 				if(input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
@@ -99,24 +107,35 @@ public class FPuntuacion extends BasicGameState{
 			}
 		}
 		
-		if(mIJ) {
+		if(!mIJ) {
 			if(tF) {
 				if(input.isKeyPressed(Input.KEY_ENTER)) {
 					jugador = nJugador.getText().toLowerCase();
-					BaseDeDatos.agregarPuntuacion(jugador, player.getScore());
-					try {
-						Thread.sleep(500);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
 					tF = false;
-					//Temporal
-					mIJ = false;
 				}
 			}
+			if(BaseDeDatos.existeJ(jugador)) {
+				mC = true;
+				if(input.getMouseX() >= 310 && input.getMouseX() <= 414 && input.getMouseY() >= 457 && input.getMouseY() <= 559 
+						&& input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
+					mC = false;
+					tF = true;
+				}else if(input.getMouseX() >= 870 && input.getMouseX() <= 977 && input.getMouseY() >= 449 && input.getMouseY() <= 554 
+						&& input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
+					mIJ = true;
+					mC = false;
+					if(BaseDeDatos.mayorP(jugador, player.getScore())) {
+					}else {
+						BaseDeDatos.agregarPuntuacion(jugador, player.getScore());
+						nB = true;
+					}
+				}
+			}else{
+				BaseDeDatos.agregarPuntuacion(jugador, player.getScore());
+				nB = true;
+				mIJ = true;
+			}
 		}
-	
 	}
 
 	@Override
